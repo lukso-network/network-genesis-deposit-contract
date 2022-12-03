efb73659be750c875273eec5fab76e3aad7433cd
Max <maximeviard@hotmail.fr>
2022-12-03 01:31:27 +0100

    refactor: merge deposit contracts

--- contracts/LUKSOGenesisDepositContract.sol
+++ contracts/LUKSOGenesisDepositContract.sol
@@ -3,7 +3,38 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-import {DepositContract} from "./DepositContract.sol";
+// This interface is designed to be compatible with the Vyper version.
+/// @notice This is the Ethereum 2.0 deposit contract interface.
+/// For more information see the Phase 0 specification under https://github.com/ethereum/eth2.0-specs
+interface IDepositContract {
+    /// @notice A processed deposit event.
+    event DepositEvent(
+        bytes pubkey,
+        bytes withdrawal_credentials,
+        bytes amount,
+        bytes signature,
+        bytes index
+    );
+
+    /// @notice Query the current deposit root hash.
+    /// @return The deposit root hash.
+    function get_deposit_root() external view returns (bytes32);
+
+    /// @notice Query the current deposit count.
+    /// @return The deposit count encoded as a little endian 64-bit number.
+    function get_deposit_count() external view returns (bytes memory);
+}
+
+// Based on official specification in https://eips.ethereum.org/EIPS/eip-165
+interface ERC165 {
+    /// @notice Query if a contract implements an interface
+    /// @param interfaceId The interface identifier, as specified in ERC-165
+    /// @dev Interface identification is specified in ERC-165. This function
+    ///  uses less than 30,000 gas.
+    /// @return `true` if the contract implements `interfaceId` and
+    ///  `interfaceId` is not 0xffffffff, `false` otherwise
+    function supportsInterface(bytes4 interfaceId) external pure returns (bool);
+}
 
 interface ERC1820Registry {
     function setInterfaceImplementer(
@@ -13,12 +44,22 @@ interface ERC1820Registry {
     ) external;
 }
 
-contract LUKSOGenesisDepositContract is DepositContract {
+contract LUKSOGenesisDepositContract is IDepositContract, ERC165 {
     address private immutable LYXeAddress;
 
     address constant registryAddress = 0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24;
     bytes32 constant TOKENS_RECIPIENT_INTERFACE_HASH =
         0xb281fc8c12954d22544db45de3159a39272895b169a852b314f9cc762e44c53b;
+    uint256 constant DEPOSIT_CONTRACT_TREE_DEPTH = 32;
+    // NOTE: this also ensures `deposit_count` will fit into 64-bits
+    uint256 constant MAX_DEPOSIT_COUNT = 2**DEPOSIT_CONTRACT_TREE_DEPTH - 1;
+    // to_little_endian_64(uint64(32 ether / 1 gwei))
+    bytes constant amount_to_little_endian_64 = hex"0040597307000000";
+
+    bytes32[DEPOSIT_CONTRACT_TREE_DEPTH] branch;
+    bytes32[DEPOSIT_CONTRACT_TREE_DEPTH] zero_hashes;
+
+    uint256 public deposit_count;
 
     /**
      * @dev Storing all the deposit data which should be sliced
@@ -55,6 +96,12 @@ contract LUKSOGenesisDepositContract is DepositContract {
             TOKENS_RECIPIENT_INTERFACE_HASH,
             address(this)
         );
+
+        // Compute hashes in empty sparse Merkle tree
+        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH - 1; height++)
+            zero_hashes[height + 1] = sha256(
+                abi.encodePacked(zero_hashes[height], zero_hashes[height])
+            );
     }
 
     /**
@@ -85,12 +132,37 @@ contract LUKSOGenesisDepositContract is DepositContract {
 
         deposit_data[deposit_count] = depositData;
 
-        deposit(
+        _deposit(
             depositData[:48],
             depositData[48:80],
             depositData[80:176],
             convertBytesToBytes32(depositData[176:208])
         );
+    }
+
+    /**
+     * @dev Freze the LUKSO Genesis Deposit Contract
+     */
+    function freezeContract() external {
+        require(msg.sender == owner, "Caller not owner");
+        contractFrozen = true;
+    }
+
+    function get_deposit_root() external view override returns (bytes32) {
+        bytes32 node;
+        uint256 size = deposit_count;
+        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
+            if ((size & 1) == 1) node = sha256(abi.encodePacked(branch[height], node));
+            else node = sha256(abi.encodePacked(node, zero_hashes[height]));
+            size /= 2;
+        }
+
+        return
+            sha256(abi.encodePacked(node, to_little_endian_64(uint64(deposit_count)), bytes24(0)));
+    }
+
+    function get_deposit_count() external view override returns (bytes memory) {
+        return to_little_endian_64(uint64(deposit_count));
     }
 
     /**
@@ -108,12 +180,79 @@ contract LUKSOGenesisDepositContract is DepositContract {
         return deposit_data[index];
     }
 
-    /**
-     * @dev Freze the LUKSO Genesis Deposit Contract
-     */
-    function freezeContract() external {
-        require(msg.sender == owner, "Caller not owner");
-        contractFrozen = true;
+    function supportsInterface(bytes4 interfaceId) external pure override returns (bool) {
+        return
+            interfaceId == type(ERC165).interfaceId ||
+            interfaceId == type(IDepositContract).interfaceId;
+    }
+
+    function _deposit(
+        bytes calldata pubkey,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signature,
+        bytes32 deposit_data_root
+    ) internal {
+        // Emit `DepositEvent` log
+        emit DepositEvent(
+            pubkey,
+            withdrawal_credentials,
+            amount_to_little_endian_64,
+            signature,
+            to_little_endian_64(uint64(deposit_count))
+        );
+
+        // Compute deposit data root (`DepositData` hash tree root)
+        bytes32 pubkey_root = sha256(abi.encodePacked(pubkey, bytes16(0)));
+        bytes32 signature_root = sha256(
+            abi.encodePacked(
+                sha256(abi.encodePacked(signature[:64])),
+                sha256(abi.encodePacked(signature[64:], bytes32(0)))
+            )
+        );
+        bytes32 node = sha256(
+            abi.encodePacked(
+                sha256(abi.encodePacked(pubkey_root, withdrawal_credentials)),
+                sha256(abi.encodePacked(amount_to_little_endian_64, bytes24(0), signature_root))
+            )
+        );
+
+        // Verify computed and expected deposit data roots match
+        require(
+            node == deposit_data_root,
+            "DepositContract: reconstructed DepositData does not match supplied deposit_data_root"
+        );
+
+        // Avoid overflowing the Merkle tree (and prevent edge case in computing `branch`)
+        require(deposit_count < MAX_DEPOSIT_COUNT, "DepositContract: merkle tree full");
+
+        // Add deposit data root to Merkle tree (update a single `branch` node)
+        deposit_count += 1;
+        uint256 size = deposit_count;
+        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
+            if ((size & 1) == 1) {
+                branch[height] = node;
+                return;
+            }
+            node = sha256(abi.encodePacked(branch[height], node));
+            size /= 2;
+        }
+        // As the loop should always end prematurely with the `return` statement,
+        // this code should be unreachable. We assert `false` just to be safe.
+        assert(false);
+    }
+
+    function to_little_endian_64(uint64 value) internal pure returns (bytes memory ret) {
+        ret = new bytes(8);
+        bytes8 bytesValue = bytes8(value);
+        // Byteswapping during copying to bytes.
+        ret[0] = bytesValue[7];
+        ret[1] = bytesValue[6];
+        ret[2] = bytesValue[5];
+        ret[3] = bytesValue[4];
+        ret[4] = bytesValue[3];
+        ret[5] = bytesValue[2];
+        ret[6] = bytesValue[1];
+        ret[7] = bytesValue[0];
     }
 
     /**
