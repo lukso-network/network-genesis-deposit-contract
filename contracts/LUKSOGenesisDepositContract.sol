31ff256fa4d697f357b3fc0750eae6dc8f1f2295
Max <maximeviard@hotmail.fr>
2022-12-05 15:30:44 +0100

    refactor: remove private keyword from TOKENS_RECIPIENT_INTERFACE_HASH

--- contracts/LUKSOGenesisDepositContract.sol
+++ contracts/LUKSOGenesisDepositContract.sol
@@ -79,7 +79,7 @@ contract LUKSOGenesisDepositContract is IDepositContract, ERC165 {
 
     address constant registryAddress = 0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24;
 
-    bytes32 private constant TOKENS_RECIPIENT_INTERFACE_HASH =
+    bytes32 constant TOKENS_RECIPIENT_INTERFACE_HASH =
         0xb281fc8c12954d22544db45de3159a39272895b169a852b314f9cc762e44c53b;
 
     uint256 constant DEPOSIT_CONTRACT_TREE_DEPTH = 32;
