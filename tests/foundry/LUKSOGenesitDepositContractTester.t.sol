1b89aaa4673b519a4b3aad0b4d2e95d838debbea
maxvia87 <maximeviard@hotmail.fr>
2023-01-13 15:41:31 +0100

    test: add foundry tests for internal functions

---
+++ tests/foundry/LUKSOGenesitDepositContractTester.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "forge-std/Test.sol";
+import './Mocks/LUKSOGenesisDepositContract.sol';
+
+
+contract LUKSOGenesitDepositContractTester is Test {
+    LUKSOGenesisDepositContractMock contractMock;
+
+    function setUp() public {
+        contractMock = new LUKSOGenesisDepositContractMock();
+    }
+
+    function testConvertBytesToBytes32(uint256 randomNumber) public {
+        bytes memory randomBytes = _getRandom208Bytes(randomNumber);
+        bytes32 outBytes32 = contractMock.convertBytesToBytes32(randomBytes);
+        assertEq(uint256(outBytes32), uint256(bytes32(randomBytes)));
+        
+    }
+
+    function testToLittleEndianValue(uint64 value) public {
+        bytes memory littleEndianBytes = contractMock.toLittleEndian64(value);
+        assertEq(littleEndianBytes.length, 8);
+
+        for (uint i = 0; i < 8; i++) {
+            require(uint64(uint8(littleEndianBytes[i])) == (value >> (i*8)) & 0xff);
+        }
+    }
+
+    function _getRandom208Bytes(uint256 randomNumber) internal  returns (bytes memory) {
+        bytes memory randomBytes = new bytes(208);
+        for (uint i = 0; i < 208; i++) {
+            randomBytes[i] = bytes1(uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, i, randomNumber)))) & 0xff);
+        }
+        return randomBytes;
+    }
+}
---
+++ tests/foundry/Mocks/LUKSOGenesisDepositContract.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+
+
+
+contract LUKSOGenesisDepositContractMock {
+    function convertBytesToBytes32(bytes memory inBytes)
+        public
+        pure
+        returns (bytes32 outBytes32)
+    {
+        bytes memory memoryInBytes = inBytes;
+        assembly {
+            outBytes32 := mload(add(memoryInBytes, 32))
+        }
+    }
+
+    function toLittleEndian64(uint64 value) public pure returns (bytes memory ret) {
+        ret = new bytes(8);
+        bytes8 bytesValue = bytes8(value);
+        // Byteswapping during copying to bytes.
+        ret[0] = bytesValue[7];
+        ret[1] = bytesValue[6];
+        ret[2] = bytesValue[5];
+        ret[3] = bytesValue[4];
+        ret[4] = bytesValue[3];
+        ret[5] = bytesValue[2];
+        ret[6] = bytesValue[1];
+        ret[7] = bytesValue[0];
+    }
+
+}
