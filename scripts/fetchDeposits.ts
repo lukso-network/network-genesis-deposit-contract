53d81923e2fe0915601e9facec4d536075dd9359
Maxime <maximeviard@hotmail.fr>
2023-03-15 12:39:14 +0000

    refactor: update fetchDeposit to generate json

--- scripts/fetchDeposits.ts
+++ scripts/fetchDeposits.ts
@@ -1,6 +1,39 @@
 import { ethers } from "hardhat";
 import fs from "fs";
 
+type DepositData = {
+  deposit_data_root: string;
+  pubkey: string;
+  signature: string;
+  amount: number;
+  version: number;
+  withdrawal_credentials: string;
+};
+
+
+function parseData(data: string[]): DepositData[] {
+  const result: DepositData[] = [];
+
+  for (let item of data) {
+    item = item.slice(2); // remove 0x
+    const pubkey = item.slice(0, 96);
+    const withdrawal_credentials = item.slice(96, 160);
+    const signature = item.slice(160, 352);
+    const deposit_data_root = item.slice(352, 416);
+
+    result.push({
+      deposit_data_root,
+      pubkey,
+      signature,
+      amount: 32000000000,
+      version: 1,
+      withdrawal_credentials,
+    });
+  }
+
+  return result;
+}
+
 async function main() {
   const depositAddress = "0x9C2Ae5bC047Ca794d9388aB7A2Bf37778f9aBA73";
 
@@ -10,9 +43,15 @@ async function main() {
   );
 
   const depositDatas = await depositContract.getDepositData();
+  const jsonData = parseData(depositDatas);
 
-  // Write the deposit data to a file
-  fs.writeFileSync("depositData.json", JSON.stringify(depositDatas));
+  fs.writeFile('depositData.json', JSON.stringify(jsonData, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
 
   console.log("Deposit data written to depositData.json");
   const numberOfDeposits = await depositContract.depositCount();
